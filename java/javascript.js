21f6642144f239a7cb95a0d63c4f364634a9e152
nursetta <n_ursetta@yahoo.com>
2016-06-29 16:04:36 -0600

    added more tiles and tiles are turning properly

--- java/javascript.js
+++ java/javascript.js
@@ -1,15 +1,10 @@
 
 window.onload = start;
 var picks = 0;
-var firstChoice;
-var secondChoice;
-var matches = 0;
-var numberOfAttempts = 0;
 var backOfCard = "?";
 var cards = document.getElementsByTagName('td');
-var tiles = ['A', 'B', 'A', 'B'];
+var tiles = ['A', 'B', 'A', 'B', 'C', 'D', 'C', 'D', 'F', 'E', 'F','E'];
 var newTiles = [];
-
 var numberOfMatchingCards = 0.5 * tiles.length;
 
 
@@ -27,31 +22,37 @@ var tile = event.target;
 //if no, do nothing.
 //shuffle cards
 	if (tile.textContent == "?") {
-	if (picks === 0 || 1) {
+	if (picks === 0 || picks == 1) {
 	var tileId = parseInt(tile.getAttribute('id'));	
     tile.textContent = tiles[tileId];
     newTiles.push(tiles[tileId]);
     newTiles.push(tileId);
-    picks ++;
+  	picks ++;
+  	console.log(newTiles);
   } 
  if (picks == 2) { 
  	check(newTiles);
- }
-}
+ 	newTiles = [];
+
+ 	}
+	}
 }
 
 function check(tileArray) {
 //if the two tiles that have been selected match,
 	console.log(newTiles[1], newTiles[3]);
-
+	console.log(newTiles[2], newTiles[4]);
 	if (tileArray[0] == tileArray[2]) {
-	
+	picks = 0;
+	return;
 	}
 
 //then leave them as they are.
 	else {
 		console.log('changing back to question mark');
 		resetTiles([tileArray[1], tileArray[3]]);
+		picks = 0;
+		newTiles = [];
 	}
 //If the two tiles dont match,
 //then change them back to question marks	
@@ -63,8 +64,10 @@ function resetTiles(tileIds) {
 		var cards = document.getElementsByTagName('td');
 		cards[tileIds[i]].textContent = '?';	
 		console.log(cards);
-		console.log(tileIds[i]);
-		picks = 0;
+		//console.log(tileIds[i]);
+		//picks = 0;
 	}
 
 }
+
+
