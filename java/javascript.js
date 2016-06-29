3ab664b9fd71fce29507c65eec8217e011608ee8
nursetta <n_ursetta@yahoo.com>
2016-06-29 17:58:06 -0600

    added an end of game functionto replay game

--- java/javascript.js
+++ java/javascript.js
@@ -6,8 +6,8 @@ var backOfCard = "?";
 var cards = document.getElementsByTagName('td');
 var tiles = ['A', 'B', 'A', 'B', 'C', 'D', 'C', 'D', 'F', 'E', 'F','E'];
 var newTiles = [];
-var numberOfMatchingCards = 0.5 * tiles.length;
-
+var matches = 0;
+var numberOfMatches = 0.5 * tiles.length;
 
 function start() {
     var cards = document.getElementsByTagName('td');
@@ -21,8 +21,6 @@ function choose(event) {
 var tile = event.target;
 //if yes, replace questionmark with tile index of element.
 //if no, do nothing.
-//shuffle cards
-//if (tile.textContent == "?") {
 	if (picks === 0 || picks == 1) {
 	var tileId = parseInt(tile.getAttribute('id'));	
     tile.textContent = tiles[tileId];
@@ -30,17 +28,15 @@ var tile = event.target;
     newTiles.push(tileId);
   	picks ++;
   	console.log(newTiles);
-  } 
+	} 
+ 
  if (picks == 2) { 
  	setTimeout(function() {
  		console.log(newTiles);
  		check(newTiles);
  		newTiles = [];
- 	}, 500);
- 
-
+ 		}, 500);
  	}
-	//}
 }
 
 function check(tileArray) {
@@ -48,10 +44,9 @@ function check(tileArray) {
 	console.log(newTiles[1], newTiles[3]);
 	console.log(newTiles[2], newTiles[4]);
 	if (tileArray[0] == tileArray[2]) {
+	matches ++;
 	picks = 0;
-	return;
 	}
-
 //then leave them as they are.
 	else {
 		console.log('changing back to question mark');
@@ -59,21 +54,20 @@ function check(tileArray) {
 		picks = 0;
 		newTiles = [];
 	}
+		if (matches == numberOfMatches) {
+		alert("You WON!" + " " + "Click OK to play Again!");
+		window.location.reload();
+	}
 //If the two tiles dont match,
 //then change them back to question marks	
 }
-
 function resetTiles(tileIds) {
 // changing text content back to question mark
-
 	for (i =0; i < tileIds.length; i++) {
 		var cards = document.getElementsByTagName('td');
 		cards[tileIds[i]].textContent = '?';	
 		console.log(cards);
-		//console.log(tileIds[i]);
-		//picks = 0;
 	}
-
 }
 
 
