eba92e74d7e964a1c33073208ae4fecf08a2ce4a
nursetta <n_ursetta@yahoo.com>
2016-06-29 09:03:06 -0600

    built reset board function and choose function. almost have them working properly.

--- java/javascript.js
+++ java/javascript.js
@@ -6,8 +6,9 @@ var secondChoice;
 var matches = 0;
 var numberOfAttempts = 0;
 var backOfCard = "?";
-
+var cards = document.getElementsByTagName('td');
 var tiles = ['A', 'B', 'A', 'B'];
+var newTiles = [];
 
 var numberOfMatchingCards = 0.5 * tiles.length;
 
@@ -17,25 +18,53 @@ function start() {
     for (var i = 0; i < cards.length; i++) {
     cards[i].addEventListener("click", choose);
   }
-choose();
 }
 
-function choose(card) {
-  if (picks == 2) {
-    //return;
-    console.log("nothing");
-  }
-  if (picks === 0) {
-    firstChoice = card;
-    card.target.textContent = tiles[0];
-    card.target.setAttribute('class', [card]);
-    picks = 1;
-    console.log("1stCard");
-  } else {
-    picks = 2;
-    secondChoice = card;
-    card.target.textContent = tiles[1];
-    card.target.setAttribute('class', [card]);
+function choose(event) {
+//check if table element is question mark.
+var tile = event.target;
+//if yes, replace questionmark with tile index of element.
+//if no, do nothing.
+//shuffle cards
+	if (tile.textContent == "?") {
+	if (picks === 0 || 1) {
+	var tileId = parseInt(tile.getAttribute('id'));	
+    tile.textContent = tiles[tileId];
+    newTiles.push(tiles[tileId]);
+    newTiles.push(tileId);
+    picks ++;
+  } 
+ if (picks == 2) { 
+ 	check(newTiles);
+ }
+}
+}
 
-  }
+function check(tileArray) {
+//if the two tiles that have been selected match,
+	console.log(newTiles[1], newTiles[3]);
+
+	if (tileArray[0] == tileArray[2]) {
+	
+	}
+
+//then leave them as they are.
+	else {
+		console.log('changing back to question mark');
+		resetTiles([tileArray[1], tileArray[3]]);
+	}
+//If the two tiles dont match,
+//then change them back to question marks	
+}
+
+function resetTiles(tileIds) {
+// changing text content back to question mark
+	for (i =0; i < tileIds.length; i++) {
+		var cards = document.getElementsByTagName('td');
+		cards[tileIds[i]].textContent = '?';	
+		console.log(cards);
+		console.log(tileIds[i]);
+		picks = 0;
+	}
+
 }
